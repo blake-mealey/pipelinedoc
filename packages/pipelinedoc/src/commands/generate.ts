cb4e6671fca39022ae552caf363193952be167e7
Blake Mealey <blakemealey@gmail.com>
2020-12-27 18:03:54 -0700

    wrote some real messy code to add warnings and errors

--- packages/pipelinedoc/src/commands/generate.ts
+++ packages/pipelinedoc/src/commands/generate.ts
@@ -3,13 +3,16 @@ import {
   generate,
   GenerateOptions,
   RepoMetaData,
+  Template,
   TemplateMetaData
 } from 'az-pipelines-documenter';
+import { getParameterList } from 'az-pipelines-documenter/src/utils/templates';
 import { glob } from 'glob';
 import { promisify } from 'util';
 import { basename } from 'path';
 import simpleGit from 'simple-git/promise';
 import GitUrlParse from 'git-url-parse';
+import { safeLoad as parseYaml } from 'js-yaml';
 
 const globAsync = promisify(glob);
 
@@ -17,10 +20,32 @@ module.exports = {
   name: 'generate',
   run: async (toolbox: GluegunToolbox) => {
     const {
-      print: { error },
+      print: {
+        error,
+        warning,
+        colors: { underline }
+      },
       parameters: { array: patterns, options },
       filesystem: { writeAsync, readAsync, path, exists }
     } = toolbox;
+
+    let hadErrors = false;
+    function padMessage(message: string) {
+      return message.replace(/\r/g, '').replace(/\n/g, '\n      ');
+    }
+
+    function trackWarning(message: string) {
+      if (options.strict) {
+        trackError(message);
+      } else {
+        warning(`WARN: ` + padMessage(message));
+      }
+    }
+
+    function trackError(message: string) {
+      error(`ERR:  ` + padMessage(message));
+      hadErrors = true;
+    }
 
     async function getGitUrl() {
       try {
@@ -79,7 +104,82 @@ module.exports = {
 
     const outputDir = options.out ?? './docs';
 
-    const ensureProperties = options.ensureProperties;
+    function assertValidProperty(
+      fileName: string,
+      properties: Partial<TemplateMetaData>,
+      property: keyof TemplateMetaData,
+      type: string | string[]
+    ) {
+      type = Array.isArray(type) ? type : [type];
+      if (!properties[property]) {
+        trackWarning(
+          `Missing property '${property}' in properties file ${underline(
+            fileName
+          )}`
+        );
+      } else if (!type.includes(typeof properties[property])) {
+        trackWarning(
+          `Property '${property}' is incorrect type (expected ${type.join(
+            '|'
+          )}) in properties file ${underline(fileName)}`
+        );
+      }
+    }
+
+    function assertValidParameters(
+      templateFileName: string,
+      propertiesFileName: string,
+      properties: Partial<TemplateMetaData>,
+      template: Template
+    ) {
+      const parametersList = getParameterList(template.parameters);
+
+      if (parametersList.length > 0) {
+        assertValidProperty(
+          propertiesFileName,
+          properties,
+          'parameters',
+          'object'
+        );
+      }
+
+      const parametersMeta = properties.parameters
+        ? Object.entries(properties.parameters)
+        : undefined;
+
+      parametersMeta?.forEach(([name, meta]) => {
+        if (!parametersList.some(param => param.name === name)) {
+          trackWarning(
+            `Parameter '${name}' from properties file ${underline(
+              propertiesFileName
+            )} does not exist in corresponding template ${underline(
+              templateFileName
+            )}`
+          );
+        } else {
+          if (!meta.description) {
+            trackWarning(
+              `Parameter '${name}' from properties file ${underline(
+                propertiesFileName
+              )} is missing property 'description'`
+            );
+          } else if (typeof meta.description !== 'string') {
+            trackWarning(
+              `Parameter '${name}' from properties file ${underline(
+                propertiesFileName
+              )} has incorrectly typed property 'description' (expected string)`
+            );
+          }
+          if (meta.format && typeof meta.format !== 'string') {
+            trackWarning(
+              `Parameter '${name}' from properties file ${underline(
+                propertiesFileName
+              )} has incorrectly typed property 'format' (expected string)`
+            );
+          }
+        }
+      });
+    }
 
     try {
       await Promise.all(
@@ -88,20 +188,56 @@ module.exports = {
           .map(async file => {
             const data = await readAsync(file);
 
-            let properties = {
+            let properties: TemplateMetaData = {
               name: basename(file.substring(0, file.lastIndexOf('.')))
             };
+
+            let template: Template | undefined;
+            try {
+              template = parseYaml(data) as Template;
+            } catch (e) {
+              throw new Error(
+                `Failed to parse YAML of template ${underline(file)}:\n${
+                  e.message
+                }`
+              );
+            }
 
             const propertiesFile =
               file.substring(0, file.lastIndexOf('.')) + '.properties.json';
             if (exists(propertiesFile) === 'file') {
-              properties = JSON.parse(await readAsync(propertiesFile));
-            } else {
-              if (ensureProperties) {
-                throw new Error(
-                  `Missing expected properties file ${propertiesFile} because --ensure-properties was specified.`
-                );
+              let fromFile: any | undefined;
+              try {
+                fromFile = JSON.parse(await readAsync(propertiesFile));
+              } catch (e) {
+                throw new Error(`Failed to load properties file ${underline(propertiesFile)}:\n${e.message}`);
               }
+              properties = {
+                name: fromFile.name,
+                description: fromFile.description,
+                parameters: fromFile.parameters,
+                version: fromFile.version
+              };
+
+              assertValidProperty(propertiesFile, properties, 'name', 'string');
+              assertValidProperty(
+                propertiesFile,
+                properties,
+                'description',
+                'string'
+              );
+              assertValidProperty(propertiesFile, properties, 'version', [
+                'string',
+                'number'
+              ]);
+
+              assertValidParameters(file, propertiesFile, properties, template);
+            } else {
+              trackWarning(
+                `Missing properties file ${underline(
+                  propertiesFile
+                )} for template ${underline(file)}`
+              );
             }
 
             const markdown = generate(
@@ -117,7 +253,11 @@ module.exports = {
           })
       );
     } catch (e) {
-      error(e.message);
+      trackError(e.message);
+    }
+
+    if (hadErrors) {
+      process.exit(1);
     }
   }
 };
